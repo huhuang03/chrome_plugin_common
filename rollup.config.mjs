4600d0ec1b0d1631b73208994c4c043e750b8a95
胡文凡 <wenfan.hu@infaith.com.cn>
2023-03-31 09:08:08 +0800

    up

---
+++ rollup.config.mjs
@@ -0,0 +1,27 @@
+import ts from 'rollup-plugin-ts'
+import { readFileSync } from "fs";
+const pkg = JSON.parse(readFileSync('package.json', {encoding: 'utf8'}));
+
+export default [
+  {
+    plugins: [
+      ts(),
+    ],
+    input: 'src/index.ts',
+    output: [
+      {
+        sourcemap: true,
+        format: 'cjs',
+        file: pkg.main,
+      },
+      {
+        format: 'es',
+        file: pkg.module
+      },
+      {
+        format: 'umd',
+        file: pkg.browser
+      }
+    ]
+  }
+]
